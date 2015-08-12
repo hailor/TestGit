1fab57ee021980bc0ce606afb50f871eedca67ac
ruanrongjun <505695253@qq.com>
2015-08-12 17:54:33 +0800

    update exam

--- Exam20150812/Exam2/src/main/java/com/hand/App.java
+++ Exam20150812/Exam2/src/main/java/com/hand/App.java
@@ -20,8 +20,8 @@ public class App {
 		Statement st = null;
 		ResultSet rs = null;
 		try {
-			String sql = "Select customer.first_name,customer.last_name,film.film_id,film.title,rental.rental_date from film,inventory,rental,payment,customer where film.film_id=inventory.film_id and inventory.inventory_id=rental.inventory_id and rental.rental_id=payment.rental_id and payment.customer_id=customer.customer_id and customer.customer_id="
-					+ input + " order by rental.rental_date desc";
+			String sql = "SELECT film.film_id,film.title,rental.rental_date,customer.first_name,customer.last_name FROM film INNER JOIN inventory ON inventory.film_id = film.film_id INNER JOIN rental ON rental.inventory_id = inventory.inventory_id INNER JOIN payment ON payment.rental_id = rental.rental_id INNER JOIN customer ON rental.customer_id = customer.customer_id AND payment.customer_id = customer.customer_id AND customer.customer_id = "
+					+ input + " ORDER BY rental.rental_date";
 			Class.forName("com.mysql.jdbc.Driver");
 			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/sakila", "root", "root");
 			st = conn.createStatement();
@@ -31,12 +31,12 @@ public class App {
 			while (rs.next()) {
 				if (isFirst) {
 					System.out.println("输出：");
-					System.out.println(rs.getString("first_name") + " " + rs.getString("last_name") + "租用的Film>>");
+					System.out.println(rs.getString("first_name") + "." + rs.getString("last_name") + "租用的Film>>");
 					System.out.println("\tFilm ID\t|\tFilm 名称\t|\t租用时间");
 					isFirst = false;
 				}
-				System.out.println(
-						"\t"+rs.getString("film_id") + "\t|\t" + rs.getString("title") + "\t|\t" + rs.getString("rental_date"));
+				System.out.println("\t" + rs.getString("film_id") + "\t|\t" + rs.getString("title") + "\t|\t"
+						+ rs.getString("rental_date"));
 			}
 
 		} catch (ClassNotFoundException e) {
